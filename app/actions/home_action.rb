ae98e29c622ce51bc0b7f5d3e346e5703d709a3f
Ivan Fomichev <ivan.fomichev@xing.com>
2012-08-30 11:13:48 +0200

    Added a front page

--- app/actions/home_action.rb
+++ app/actions/home_action.rb
@@ -1,36 +1,8 @@
-require 'eventmachine-tail'
-
+require 'erb'
 class HomeAction < Cramp::Action
-  self.transport = :long_polling
-
-  on_start :setup_timeout
-  on_start :subscribe
-
-  on_finish :unsubscribe
-
-  @@channel = EventMachine::Channel.new
-  @@filetail = EventMachine::file_tail('/tmp/ponytail') do |filetail, line|
-    @@channel.push line
-  end
-
-  def setup_timeout
-    @timer = EventMachine::Timer.new(10) do
-      render 'timeout'
-    end
-  end
-
-  def subscribe
-    @sid = @@channel.subscribe do |message|
-      @timer.cancel
-      render message
-    end
-  end
-
-  def unsubscribe
-    @@channel.unsubscribe(@sid) if @sid
-  end
-
-  def respond_with
-    [200, {'Content-Type' => 'text/plain'}]
+  def start
+    template = ERB.new(File.read(Ponytail::Application.root('app/views/index.erb')))
+    render template.result(binding)
+    finish
   end
 end
---
+++ app/actions/tail_action.rb
@@ -0,0 +1,36 @@
+require 'eventmachine-tail'
+
+class TailAction < Cramp::Action
+  self.transport = :long_polling
+
+  on_start :setup_timeout
+  on_start :subscribe
+
+  on_finish :unsubscribe
+
+  @@channel = EventMachine::Channel.new
+  @@filetail = EventMachine::file_tail('/tmp/ponytail') do |filetail, line|
+    @@channel.push line
+  end
+
+  def setup_timeout
+    @timer = EventMachine::Timer.new(10) do
+      render 'timeout'
+    end
+  end
+
+  def subscribe
+    @sid = @@channel.subscribe do |message|
+      @timer.cancel
+      render message
+    end
+  end
+
+  def unsubscribe
+    @@channel.unsubscribe(@sid) if @sid
+  end
+
+  def respond_with
+    [200, {'Content-Type' => 'text/plain'}]
+  end
+end
